6a4d2ffe0720df6113db667abdf386b363c97fcf
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-08-22 00:19:59 +0300

    Printf

---
+++ base_converters.c
@@ -0,0 +1,103 @@
+#include "main.h"
+
+unsigned int conv_sbase(buff_t *output, long int numm, char *base,
+		unsigned char flags, int width, int p);
+unsigned int conv_ubase(buff_t *output,
+		unsigned long int numm, char *base,
+		unsigned char flags, int width, int p);
+
+/**
+ * conv_sbase - Converts a signed long to an inputted base and stores
+ *                 the result to a buffer contained in a struct.
+ * @output: A buffer_t struct containing a character array.
+ * @numm: A signed long to be converted.
+ * @base: A pointer to a string containing the base to convert to.
+ * @flags: Flag modifiers.
+ * @width: A width modifier.
+ * @p: A precision modifier.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int conv_sbase(buff_t *output, long int numm, char *base,
+		unsigned char flags, int width, int p)
+{
+	int sz;
+	char digit, pad = '0';
+	unsigned int rt = 1;
+
+	for (sz = 0; *(base + sz);)
+		sz++;
+
+	if (numm >= sz || numm <= -sz)
+		rt += conv_sbase(output, numm / sz, base,
+				flags, width - 1, p - 1);
+
+	else
+	{
+		for (; p > 1; p--, wid--) /* Handle precision */
+			rt += _memcpy(output, &pad, 1);
+
+		if (NEG_FLAG == 0) /* Handle width */
+		{
+			pad = (ZERO_FLAG == 1) ? '0' : ' ';
+			for (; width > 1; width--)
+				rt += _memcpy(output, &pad, 1);
+		}
+	}
+
+	digit = base[(numm < 0 ? -1 : 1) * (numm % sz)];
+	_memcpy(output, &digit, 1);
+
+	return (rt);
+}
+
+/**
+ * conv_ubase - Converts an unsigned long to an inputted base and
+ *                 stores the result to a buffer contained in a struct.
+ * @output: A buffer_t struct containing a character array.
+ * @numm: An unsigned long to be converted.
+ * @base: A pointer to a string containing the base to convert to.
+ * @flags: Flag modifiers.
+ * @width: A width modifier.
+ * @p: A precision modifier.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int conv_ubase(buff_t *output, unsigned long int numm, char *base,
+		unsigned char flags, int width, int p)
+{
+	unsigned int sz, rt = 1;
+	char digit, pad = '0', *lead = "0x";
+
+	for (sz = 0; *(base + sz);)
+		sz++;
+
+	if (numm >= sz)
+		rt += conv_ubase(output, numm / sz, base,
+				flags, width - 1, p - 1);
+
+	else
+	{
+		if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
+		{
+			width -= 2;
+			p -= 2;
+		}
+		for (; p > 1; p--, width--) /* Handle precision */
+			rt += _memcpy(output, &pad, 1);
+
+		if (NEG_FLAG == 0) /* Handle width */
+		{
+			pad = (ZERO_FLAG == 1) ? '0' : ' ';
+			for (; width > 1; width--)
+				rt += _memcpy(output, &pad, 1);
+		}
+		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
+			rt += _memcpy(output, lead, 2);
+	}
+
+	digit = base[(numh % sz)];
+	_memcpy(output, &digit, 1);
+
+	return (rt);
+}
--- main.h
+++ main.h
@@ -81,20 +81,20 @@ unsigned int (*handle_specifiers(const char *spec))(va_list, buff_t *,
 		unsigned char, int, int, unsigned char);
 
 /* Modifiers */
-unsigned int print_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
-unsigned int print_string_width(buffer_t *output,
-		unsigned char flags, int wid, int prec, int size);
-unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
+unsigned int print_width(buff_t *output, unsigned int printed,
+		unsigned char flags, int width);
+unsigned int print_string_width(buff_t *output,
+		unsigned char flags, int width, int p, int sz);
+unsigned int print_neg_width(buff_t *output, unsigned int printed,
+		unsigned char flags, int width);
 
 /* Helper Functions */
-buffer_t *init_buffer(void);
-void free_buffer(buffer_t *output);
-unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
-unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-		unsigned char flags, int wid, int prec);
-unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
-		unsigned char flags, int wid, int prec);
+buff_t *init_buffer(void);
+void free_buffer(buff_t *output);
+unsigned int _memcpy(buff_t *output, const char *src, unsigned int k);
+unsigned int convert_sbase(buff_t *output, long int numm, char *base,
+		unsigned char flags, int width, int p);
+unsigned int convert_ubase(buff_t *output, unsigned long int numm, char *base,
+		unsigned char flags, int width, int p);
 
 #endif
